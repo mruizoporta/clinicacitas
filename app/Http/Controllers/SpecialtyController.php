7dcd4796d331811436690053f7f3b0a78bff52ce
mruizoporta <mruiz.oporta@gmail.com>
2023-05-15 23:36:27 -0600

    Control de citas

---
+++ app/Http/Controllers/SpecialtyController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SpecialtyController extends Controller
+{
+    public function __construct(){
+        $this-> middleware('auth');
+    }
+    public function index(){
+
+        return view('specialties.index');
+    }
+}
